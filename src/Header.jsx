42f62b26ad2119b5a2d0af0253185d414bba7829
Bharat <rajbanshibharat60@gmail.com>
2024-11-15 16:17:09 +0545

    final commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs'
+import React from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
 const Header = () => {
   return (
-<header className='header'>
-<div className='menu-icon'>
-    <BsJustify className="icon"/>
-</div>
-<div className='header-left'>
-    <BsSearch className="icon"/>
-</div>
-<div className='header-right'>
-    <BsFillBellFill className="icon"/>
-    <BsFillEnvelopeFill className="icon"/>
-    <BsPersonCircle className="icon"/>
-</div>
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" />
+      </div>
+      <div className="header-left">
+        <BsSearch className="icon" />
+      </div>
+      <div className="header-right">
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+      </div>
+    </header>
+  );
+};
 
-</header>  )
-}
-
-export default Header
+export default Header;
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,70 +1,84 @@
-import React from 'react'
-import { BsCart3 , BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill,
-    BsListCheck, BsMenuButtonWideFill, BsFillGearFill
- } from 'react-icons/bs'
+import React from "react";
+import {
+  BsCart3,
+  BsGrid1X2Fill,
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsListCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from "react-icons/bs";
 const Sidebar = () => {
   return (
-<>
-<aside id="sidebar">
-<div className='sidebar-title'>
-    <div className='sidebar-brand'>
-        <BsCart3 className="icon_header"/>Shop
+    <>
+      <aside id="sidebar">
+        <div className="sidebar-title">
+          <div className="sidebar-brand">
+            <BsCart3 className="icon_header" />
+            Shop
+          </div>
+          <span className="icon_close_icon">X</span>
+        </div>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsGrid1X2Fill className="icon" />
+              Dashboard
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsFillArchiveFill className="icon" />
+              Products
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsFillGrid3X3GapFill className="icon" />
+              Categories
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsPeopleFill className="icon" />
+              Customers
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsListCheck className="icon" />
+              Inventory
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsMenuButtonWideFill className="icon" />
+              Reports
+            </a>
+          </li>
+        </ul>
+        <ul className="sidebar_list">
+          <li className="sidebar-list-item">
+            <a href="">
+              <BsFillGearFill className="icon" />
+              Setting
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+};
 
-    </div>
-<span className='icon_close_icon'>X</span>
-</div>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsGrid1X2Fill className="icon"/>Dashboard
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsFillArchiveFill className="icon"/>Products
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsFillGrid3X3GapFill className="icon"/>Categories
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsPeopleFill className="icon"/>Customers
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            < BsListCheck className="icon"/>Inventory
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsMenuButtonWideFill className="icon"/>Reports
-        </a>
-    </li>
-</ul>
-<ul className='sidebar_list'>
-    <li className='sidebar-list-item'>
-        <a href="">
-            <BsFillGearFill className="icon"/>Setting
-        </a>
-    </li>
-</ul>
-
-</aside>
-</>  )
-}
-
-export default Sidebar
+export default Sidebar;
